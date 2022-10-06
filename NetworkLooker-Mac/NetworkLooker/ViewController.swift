510931e5feb6f73f71cacb9e67345b9f340f2714
Eric <13197616+ericide@users.noreply.github.com>
2022-10-06 14:49:57 +0800

    update

--- NetworkLooker-Mac/NetworkLooker/ViewController.swift
+++ NetworkLooker-Mac/NetworkLooker/ViewController.swift
@@ -7,6 +7,7 @@
 
 import Cocoa
 import WebKit
+import Socket
 
 class ViewController: NSViewController {
 
@@ -14,6 +15,42 @@ class ViewController: NSViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        do {
+            let socket = try Socket.create(family: .unix, type: .stream, proto: .tcp)
+            print("1")
+            try socket.connect(to: "/Users/wu/Downloads/my.sock")
+            print("2")
+            
+            let queue = DispatchQueue.global(qos: .default)
+            queue.async { [unowned self, socket] in
+                var readData = Data(capacity: 4096)
+                do {
+                                // Write the welcome string...
+                                try socket.write(from: "GET /?key=value&key=value HTTP/1.1\nHost:www.baidu.com\nAccept-Language: zh-CN,zh;q=0.9\n\r\n\r")
+                                
+                                repeat {
+                                    let bytesRead = try socket.read(into: &readData)
+                                    if bytesRead > 0 {
+                                        guard let response = String(data: readData, encoding: .utf8) else {
+                                            
+                                            print("Error decoding response...")
+                                            readData.count = 0
+                                            break
+                                        }
+                                        print(response)
+                                    } else {
+                                        break
+                                    }
+                                } while true
+                } catch {
+                    print(error)
+                }
+            }
+            
+            
+        } catch {
+            print(error)
+        }
         
 
         
@@ -28,8 +65,8 @@ class ViewController: NSViewController {
         
         let content = try! String.init(contentsOf: URL.init(fileURLWithPath: path!))
         
-        webView.loadHTMLString(content, baseURL: nil)
-        
+//        webView.loadHTMLString(content, baseURL: nil)
+        webView.load(URLRequest(url: URL(string: "http://localhost:3000/")!))
         // Do any additional setup after loading the view.
     }
 
