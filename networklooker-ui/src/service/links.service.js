6c6a2b0a6b0c5a90f5c0e4666920bc14c65e8519
Eric <13197616+ericide@users.noreply.github.com>
2022-11-03 12:58:45 +0800

    Add request method

--- networklooker-ui/src/service/links.service.js
+++ networklooker-ui/src/service/links.service.js
@@ -1,9 +1,9 @@
 import {fetch} from "./base.service";
 
 export const allLinks = (params) => {
-    return fetch("links", params)
+    return fetch("links", "GET", params)
 }
 
 export const linkDetail = (params) => {
-    return fetch("link", params)
+    return fetch("link", "GET",params)
 }
