d85ec3aac7703b5468c065582f3c890c8597b54d
Eric <13197616+ericide@users.noreply.github.com>
2022-11-06 02:09:58 +0800

    Rewrite request tree view

--- networklooker-ui/src/view/monitor/request_tree.js
+++ networklooker-ui/src/view/monitor/request_tree.js
@@ -1,76 +1,46 @@
-import React, { useState} from 'react';
+import React, { useState } from "react";
 
-export const RequestTree = ({dataSource, onClick}) => {
+export const RequestTree = ({ dataSource, onClick }) => {
+  const [treeData, setTreeData] = useState([]);
 
-    const [treeData, setTreeData] = useState([]);
+  const [expandedKeys, setExpandedKeys] = useState(["api.latibac.com"]);
 
-    // useEffect(()=>{
-    //
-    //     let dataList = []
-    //
-    //
-    //
-    //
-    //     for (const item of dataSource) {
-    //
-    //         const pathes = item.path.split("/").filter(entry => entry !== "");
-    //         let pathes2 = [item.host]
-    //         pathes2.push(...pathes)
-    //         item.parts = pathes2
-    //         dataList.push(item)
-    //
-    //
-    //
-    //
-    //         // let array = [item.host, ]
-    //         //
-    //         //
-    //         //
-    //         // dataList.findIndex((value)=>{
-    //         //
-    //         // })
-    //     }
-    //     let node = makePathsTree(dataList)
-    //
-    //     setTreeData(node.children)
-    //     // console.log(node)
-    // },[dataSource])
+  const onExpand = (items) => {
+    // console.log("onExpand", item, expandedKeys)
+    setExpandedKeys(items);
+  };
 
+  const autoExpandParent = true;
 
-
-
-    const [expandedKeys, setExpandedKeys] = useState(["api.latibac.com"]);
-
-    const onExpand = (items)=>{
-        // console.log("onExpand", item, expandedKeys)
-        setExpandedKeys(items)
+  const onSelect = (selectedKeys, e) => {
+    if (e.node.keyId !== null) {
+      onClick(e.node.keyId);
+    } else {
+      let items = expandedKeys;
+      const i = items.indexOf(e.node.key);
+      if (i < 0) {
+        items.push(e.node.key);
+      } else {
+        items.splice(i, 1);
+      }
+      console.log(items);
+      setExpandedKeys(items);
     }
+  };
 
-    const autoExpandParent = true
-
-    const onSelect = (selectedKeys, e) => {
-        if (e.node.keyId !== null) {
-            onClick(e.node.keyId)
-        } else {
-            let items = expandedKeys
-            const i = items.indexOf(e.node.key)
-            if (i < 0) {
-                items.push(e.node.key)
-            } else {
-                items.splice(i, 1)
-            }
-            console.log(items)
-            setExpandedKeys(items)
-        }
-    }
-
-
-
-    return <>
-        <ul style={{width: "100%", height: "100%"}}>
-            {dataSource.map((item) => {
-                return <li onClick={e=>onClick(item.id)} key={item.id}><a style={{color: "black"}}>{item.host}</a></li>
-            })}
-        </ul>
-    </>
-}
+  return (
+    <div style={{ width: "100%" }}>
+      {dataSource.map((item) => {
+        return (
+          <div
+            style={{ backgroundColor: "green", margin: "8px" }}
+            onClick={(e) => onClick(item.id)}
+            key={item.id}
+          >
+            <a style={{ color: "black" }}>{item.host + item.uri}</a>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
