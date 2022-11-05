6464f88fdd0916abda3106e6acec99330902ea90
Eric <13197616+ericide@users.noreply.github.com>
2022-11-06 02:08:02 +0800

    Rewrite links api path

--- networklooker-ui/src/service/links.service.js
+++ networklooker-ui/src/service/links.service.js
@@ -1,9 +1,9 @@
 import {fetch} from "./base.service";
 
 export const allLinks = (params) => {
-    return fetch("api/links", "GET", params)
+    return fetch("api/connection/requests", "GET", params)
 }
 
 export const linkDetail = (params) => {
-    return fetch("api/link", "GET",params)
+    return fetch("api/connection/request", "GET",params)
 }
