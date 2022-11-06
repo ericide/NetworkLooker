581530648d0c2156d76aca9aa5ee15961677d448
Eric <13197616+ericide@users.noreply.github.com>
2022-11-06 17:37:07 +0800

    Add config path parameter

--- NetworkLooker-Mac/NetworkLooker/NetworkLooker.h
+++ NetworkLooker-Mac/NetworkLooker/NetworkLooker.h
@@ -68,7 +68,7 @@ typedef struct { void *data; GoInt len; GoInt cap; } GoSlice;
 extern "C" {
 #endif
 
-extern void start(char* sockPath);
+extern void start(char* sockPath, char* configPath);
 
 #ifdef __cplusplus
 }
